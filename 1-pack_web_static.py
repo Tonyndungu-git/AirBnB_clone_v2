8f5258acbb0dcbf48c003bbc207352da0840b0ad
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-09 22:27:13 +0300

    generating a tgz file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,14 +10,13 @@ import os.path
 
 
 def do_pack():
-    """
-    generate tgz archive using fabric
-    """
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_{}.tgz".format(date)
-    if os.path.isdir("versions") is False:
-        local(" mkdir versions")
-    local('tar -cvzf ' + file_path + ' web_static')
-    if os.path.exists(file_path):
-        return file_path
-    return None
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
